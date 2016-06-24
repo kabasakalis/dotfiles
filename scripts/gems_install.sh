a87ec399cb683659c32acaf378a7f6e4af04b514
Spiros Kabasakalis <kabasakalis@gmail.com>
2016-06-25 01:06:40 +0300

    Added trash-cli aliases

--- scripts/gems_install.sh
+++ scripts/gems_install.sh
@@ -1,1 +1,1 @@
-gem install rubocop rubocop-rspec starscope ruby-beautify haml-lint scss-lint mdl && npm install js-beautify && install exuberant-ctags
+gem install rubocop rubocop-rspec starscope ruby-beautify haml-lint scss-lint mdl && npm install js-beautify && install exuberant-ctags && install xclip
